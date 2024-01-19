07200aa31492420ae5e446f06e3c8ca834e163bf
stormytuna <stormytuna@outlook.com>
2024-01-19 20:20:13 +0000

    Add project files.

---
+++ ReiChiquita/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using LethalLib;
+using UnityEngine;
+
+namespace ReiChiquita
+{
+	[BepInPlugin(ModGUID, ModName, ModVersion)]
+	[BepInDependency(Plugin.ModGUID)]
+	public class ReiChiquitaBase : BaseUnityPlugin
+	{
+		public const string ModGUID = "stormytuna.ReiChiquita";
+		public const string ModName = "ReiChiquita";
+		public const string ModVersion = "1.0.0";
+
+		public static ManualLogSource Log = BepInEx.Logging.Logger.CreateLogSource(ModGUID);
+		public static ReiChiquitaBase Instance;
+		public static AssetBundle CustomAssets;
+	}
+}
